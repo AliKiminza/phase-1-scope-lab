1520a3ecc2dc63c23ee2c9f05f1286be3b603d6a
alikiminza <ali.kiminza@student.moringaschool.com>
2023-06-19 08:24:01 +0300

    scope lab modified

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+var customerName;
+customerName='bob';
+const leastFavouriteCustomer = 'Jacob';
+
+function fixTheScope(){
+    return customerName;
+}
+console.log (fixTheScope())
+
+function upperCaseCustomerName(){
+   const customerName=customerName.toUpperCase();
+    return customerName;
+}
+//console.log (upperCaseCustomerName())
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+console.log (setBestCustomer(bestCustomer))
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+console.log (overwriteBestCustomer())
+
+function changeLeastFavoriteCustomer(){
+    leastFavouriteCustomer = 'Ali';
+    return leastFavouriteCustomer;
+}
